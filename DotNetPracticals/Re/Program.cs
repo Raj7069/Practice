0802bd6f8527e34ae4e1ea6feafc3da07e462136
RAJ Parmar <rajpparmar1999@gmail.com>
2020-01-20 13:23:03 +0530

    DotNet Practicals

---
+++ DotNetPracticals/Re/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Re
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name, Country;
+            Console.WriteLine("Please, Enter your Name:-");
+            Name = Console.ReadLine();
+            Console.WriteLine("Please, Enter your Country:-");
+            Country= Console.ReadLine();
+
+            Console.WriteLine("Hello, Your Name is "+Name+" and you from Country "+Country);
+            Console.Read();
+        }
+    }
+}
